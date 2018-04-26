bb33272e527bd35c2a441a5385e7043846c844b4
shadowmonkey95 <vuphamas@gmail.com>
2018-04-26 21:54:58 +0700

    Update sortable-table

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,16 +2,43 @@ class MainController < ApplicationController
   def index
   end
 
-  def search
-    @shops = Shop.ransack(user_id_in: [current_user.id], name_cont: params[:q]).result(distinct: true)
-    @requests = Request.ransack(user_id_in: [current_user.id], name_cont: params[:q]).result(distinct: true)
+  # def search
+  #   @shops = Shop.ransack(user_id_in: [current_user.id], name_cont: params[:q]).result(distinct: true)
+  #   @requests = Request.ransack(user_id_in: [current_user.id], name_cont: params[:q]).result(distinct: true)
+  #
+  #   respond_to do |format|
+  #     format.html {}
+  #     format.json {
+  #       @shops = @shops.limit(5)
+  #       @requests = @requests.limit(5)
+  #     }
+  #   end
+  # end
 
-    respond_to do |format|
-      format.html {}
-      format.json {
-        @shops = @shops.limit(5)
-        @requests = @requests.limit(5)
-      }
+  def search
+    if current_user.has_role? :admin
+      @shops = Shop.ransack(name_cont: params[:q]).result(distinct: true)
+      @requests = Request.ransack(name_cont: params[:q]).result(distinct: true)
+
+      respond_to do |format|
+        format.html {}
+        format.json {
+          @shops = @shops.limit(5)
+          @requests = @requests.limit(5)
+        }
+      end
+    else
+      @shops = Shop.ransack(user_id_in: [current_user.id], name_cont: params[:q]).result(distinct: true)
+      @requests = Request.ransack(user_id_in: [current_user.id], name_cont: params[:q]).result(distinct: true)
+
+      respond_to do |format|
+        format.html {}
+        format.json {
+          @shops = @shops.limit(5)
+          @requests = @requests.limit(5)
+        }
+      end
     end
+
   end
 end
