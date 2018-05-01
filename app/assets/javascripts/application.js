6930c7e9b322e9ee0cf9493ee18ce6f65d65d8db
shadowmonkey95 <vuphamas@gmail.com>
2018-05-02 03:04:13 +0700

    reorder js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require rails-ujs
 //= require jquery3
 //= require jquery
+//= require popper
+//= require bootstrap
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require underscore
