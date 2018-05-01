9762484c4febda6b4f3c165ae5441ded4d725d4d
Bui Dang Minh <minhbui12121995@gmail.com>
2018-05-01 14:39:54 +0700

    get path by shipper id api

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -2,12 +2,18 @@ class PathsController < ApplicationController
 
   def show
     path = Path.find_by(shipper_id: params[:id])
-    render json: {
-        message: 'success',
-        data: {
-          path: path.path
-        }
-    }, status: :ok
+    if path
+      render json: {
+          message: 'success',
+          data: {
+            path: path.path
+          }
+      }, status: :ok
+    else
+      render json: {
+          message: 'error',
+      }, status: :ok
+    end
   end
 
 end
